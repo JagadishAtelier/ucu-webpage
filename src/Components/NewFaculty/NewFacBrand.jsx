2f58ec5ddfc800dc97ec61a4f5ac92895bd2bea1
JagadishRamakrishnan <jeevasumathi1357@gmail.com>
2025-09-12 10:40:29 +0530

    updated new faculty page

--- src/Components/NewFaculty/NewFacBrand.jsx
+++ src/Components/NewFaculty/NewFacBrand.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import bgImage from '../../Assets/aboutPageImage/image-ucu.svg'
 function NewFacBrand() {
   return (
-    <div className='d-flex flex-column flex-lg-column align-items-start justify-content-center justify-content-lg-start text-white text-center text-lg-start py-4 px-3 p-lg-5 p-lg-7 bg-darkblue my-lg-5 my-5'
+    <div className='NFB-overlay d-flex flex-column flex-lg-column align-items-start justify-content-center justify-content-lg-start text-white text-center text-lg-start py-4 px-3 p-lg-5 p-lg-7 bg-darkblue my-lg-5 my-5'
     style={{
-      backgroundImage : `url(${bgImage})`,
+      backgroundImage : `url("https://png.pngtree.com/thumb_back/fh260/background/20240917/pngtree-a-lecture-hall-or-meeting-space-in-an-upscale-college-business-image_16223871.jpg")`,
       backgroundPosition : "center",
       backgroundSize : "cover",
       backgroundRepeat : "no-repeat",
-      minHeight : "20vh"
+      minHeight : "30vh"
 
   }}
     >
