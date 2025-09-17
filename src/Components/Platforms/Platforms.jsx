0c50eddfa891d4b23647fe8ea13fe91adb50ea5d
JagadishRamakrishnan <jeevasumathi1357@gmail.com>
2025-09-17 10:58:07 +0530

    updated logo

--- src/Components/Platforms/Platforms.jsx
+++ src/Components/Platforms/Platforms.jsx
@@ -12,35 +12,35 @@ const data = [
   },
   {
     head: "Product Management",
-    text: "Enhance skills through online learning with expert guidance and mastery in digital threat defense.",
+    text: "Product management bridges business, technology, and customers—driving strategy, defining requirements, prioritizing features, and ensuring successful product delivery.",
   },
   {
     head: "Cybersecurity",
-    text: "Build AI skills through online platforms and drive smarter innovation.",
+    text: "Cybersecurity safeguards digital systems, networks, and data—preventing unauthorized access, mitigating threats, ensuring privacy, and maintaining trust in technology.",
   },
   {
     head: "Digital Marketing",
-    text: "Master online product lifecycles through agile tools and market-driven decisions.",
+    text: "Digital marketing promotes brands online—using SEO, social media, content, email, and analytics to engage audiences, drive traffic, and boost sales.",
   },
   {
     head: "Mobilty & Sustainability",
-    text: "Learn marketing online with interactive modules, real-time analytics, and growth-focused strategies.",
+    text: "Mobility & sustainability focus on eco-friendly transport, renewable energy, smart infrastructure, and innovation—reducing emissions, conserving resources, and ensuring greener futures.",
   },
   {
     head: "Consulting",
-    text: "Learn online to design green solutions with smart and sustainable transport technology.",
+    text: "Consulting provides expert advice, strategic guidance, and problem-solving—helping organizations improve performance, efficiency, innovation, and achieve sustainable growth successfully.",
   },
   {
     head: "Banking & Finance",
-    text: "Learn online to drive business transformation with problem-solving and stakeholder collaboration.",
+    text: " Banking & finance manage money, investments, and risks—supporting businesses, individuals, and economies through services, innovation, security, and sustainable growth.",
   },
   {
     head: "Business Analytics",
-    text: "Learn online to optimize finance with risk management, investment, and compliance skills.",
+    text: "Business analytics interprets data, identifies patterns, and delivers insights—supporting strategic decisions, improving efficiency, forecasting trends, and driving business growth.",
   },
   {
     head: "Data Science",
-    text: "Learn online to drive product success with positioning, launches, and performance analytics.",
+    text: "Data science combines statistics, programming, and domain knowledge—analyzing data, building models, uncovering insights, and enabling smarter, evidence-based decisions.",
   },
 ];
 
