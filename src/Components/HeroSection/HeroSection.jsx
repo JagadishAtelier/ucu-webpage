aa37b867c301e830bb44eb4bd017fa44ec413ae4
JagadishRamakrishnan <jeevasumathi1357@gmail.com>
2025-09-24 16:32:24 +0530

    updated UCUbanner images

--- src/Components/HeroSection/HeroSection.jsx
+++ src/Components/HeroSection/HeroSection.jsx
@@ -15,46 +15,47 @@ import banner2 from '../../Assets/aac/2.png'
 import banner3 from '../../Assets/aac/Banner3.png'
 import banner4 from '../../Assets/aac/4.png'
 import banner8 from '../../Assets/aac/8.png'
+import bannerImage from '../../Assets/aac/FinalBannerImage.png'
 const heroData = [
   {
-    header: <>Guided by Industry &<span className="color-highlight">Academic </span> Icons</>,
+    header: <>Guided by Industry & <span className="color-highlight"> Academic </span> Icons</>,
     para: "Every program is co-created and co-delivered by academia and industry—where faculty and industry CXOs collaborate to build future-ready, job-relevant learning experiences for students",
-    images: banner1,
+    images: bannerImage,
   },
   {
     header: <>Future Ready Programs for <span className="color-highlight">Emerging Industry</span> Sectors</>,
     para: "From GCC and FinTech to Healthcare and Semiconductor industries, our specialised programs are expertly tailored to empower talent to lead innovation and deliver transformative strategies from day one.",
-    images: banner2,
+    images: bannerImage,
   },
   {
     header: <><span className="color-highlight">Business Advisory </span>Council (BAC)</>,
     para: "Our Business Advisory Council (BAC) anchors UCU’s strategic vision—shaping curriculum, guiding long-term direction, and ensuring every program delivers tangible career value from day one",
-    images: banner3,
+    images: bannerImage,
   },
   {
     header: <><span className="color-highlight">Academic Advisory </span>Council (AAC)</>,
     para: "UCU’s Academic Advisory Council (AAC) strengthens our intellectual core—shaping curriculum, deepening academic rigor, and laying the foundation of business knowledge to nurture future leaders",
-    images: banner4,
+    images: bannerImage,
   },
   {
     header: <><span className="color-highlight">CHRO Advisory </span>Council (CHRAC)</>,
     para: "UCU’s CHRO Advisory Council brings strategic HR minds together to shape talent frameworks, leadership readiness, and workforce evolution—ensuring our programs mirror the priorities of enterprise people strategy.",
-    images: image2,
+    images: bannerImage,
   },
   {
     header: <><span className="color-highlight">Talent Acquisition </span>Council (TAC)</>,
     para: "UCU’s Talent Acquisition Council draws on frontline recruitment expertise to align hiring realities with learner outcomes—crafting career pathways that meet the pulse of industry demand",
-    images: image2,
+    images: bannerImage,
   },
   {
     header: <><span className="color-highlight">L&D Advisory </span>Council (L&DAC)</>,
     para: "UCU’s L&D Advisory Council collaborates with learning leaders to refine instructional design, enhance learner engagement, and embed growth mindsets into every stage of professional development",
-    images: image2,
+    images: bannerImage,
   },
   {
     header: <><span className="color-highlight">Young CXO </span>Council (YCC)</>,
     para: " UCU’s Young CXO Council (YCC) connects tomorrow’s talent with today’s trailblazing leaders—driving measurable corporate impact and channeling boardroom insights directly into our classrooms",
-    images: banner8,
+    images: bannerImage,
   },
 ];
 
