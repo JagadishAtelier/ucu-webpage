620344caec5fa1c318d92461a83e8ce04c0d287a
prasanthoyasco <prasanthoyasco@gmai.com>
2025-10-07 18:43:30 +0530

    about page added

--- src/Pages/AboutPage/Timeline.jsx
+++ src/Pages/AboutPage/Timeline.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col  } from "react-bootstrap";
-import "./timeline.css";
+import "./Timeline.css";
 import TimelineRow from "./TimelineRow";
 
 // ✅ TimelineRow Component
