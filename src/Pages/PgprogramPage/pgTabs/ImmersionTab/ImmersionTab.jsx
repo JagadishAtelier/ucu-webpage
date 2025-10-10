17ca95b6bbea1b0da921baaa90027a9705d78b2a
JagadishAtelier <jagadish.atelier@gmail.com>
2025-10-10 11:42:35 +0530

    updated font family and apply now now btn style in platform sections

---
+++ src/Pages/PgprogramPage/pgTabs/ImmersionTab/ImmersionTab.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './ImmersionTab.css'
+function ImmersionTab() {
+  return (
+    <div>
+        PGDM international immersion
+    </div>
+  )
+}
+
+export default ImmersionTab
