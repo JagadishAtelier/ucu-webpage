e4562c3afcaa74c2fb21e3514496044711b30452
JagadishAtelier <jagadish.atelier@gmail.com>
2025-10-13 14:15:03 +0530

    updated overview tab

--- src/Pages/PgprogramPage/pgTabs/OverviewTab/OverviewParticipant.jsx
+++ src/Pages/PgprogramPage/pgTabs/OverviewTab/OverviewParticipant.jsx
@@ -11,45 +11,31 @@ import { Navigation, Pagination, Autoplay } from "swiper/modules";
 const data = [
   {
     videoUrl: "https://youtu.be/uvyTVoElld4?si=ET5MBHw44ReoNMhG",
-    thumbnail: "https://www.spjimr.org/wp-content/uploads/2022/07/Amardeep-Singh-jpg.webp",
+    thumbnail: "https://t3.ftcdn.net/jpg/03/88/72/64/360_F_388726457_kjWTTE4phvKdYjSVKr13N1lA1mKc4yJe.jpg",
     author: "Rajesh Mehra",
     proffection: "CEO, InnovateCorp",
     para: "UCU is bridging the gap between academia and industry with programs that prepare future-ready leaders",
   },
   {
     videoUrl: "https://youtu.be/bhgVNTJUytA?si=FGAtsM2xOS8NPyRU",
-    thumbnail: "https://www.spjimr.org/wp-content/uploads/2022/09/PGP-GM-1.jpg",
+    thumbnail: "https://images.pexels.com/photos/1587009/pexels-photo-1587009.jpeg?cs=srgb&dl=pexels-moose-photos-1587009.jpg&fm=jpg",
     author: "Anita Kapoor",
     proffection: "Managing Director, Visionary Enterprises",
     para: "The corporate partnership initiatives at UCU are creating impactful opportunities for students and professionals alike.",
   },
   {
     videoUrl: "https://youtu.be/ulaQhIpWY98?si=O_Ofh6g_EBIawX-C",
-    thumbnail: "https://www.spjimr.org/wp-content/uploads/2022/09/PGP-GM-2.jpg",
+    thumbnail: "https://th.bing.com/th/id/R.3bcbeff4ee0abb81ef150c9ea7e35730?rik=t3aMo1m4uUQi6g&riu=http%3a%2f%2fwww.newdesignfile.com%2fpostpic%2f2010%2f05%2ffree-stock-photos-people_102217.jpg&ehk=vGjIrntn5QyP%2fIXY2Ei7Iiz4%2fy%2byXvP8I8j0XxemwjI%3d&risl=&pid=ImgRaw&r=0",
     author: "Vikram Sethi",
     proffection: "Chief Human Resources Officer, Global Dynamics",
     para: "UCU is cultivating the next generation of business leaders through practical, industry-focused learning.",
   },
   {
     videoUrl: "https://youtu.be/uvyTVoElld4?si=ET5MBHw44ReoNMhG",
-    thumbnail: "https://www.spjimr.org/wp-content/uploads/2022/07/Amardeep-Singh-jpg.webp",
+    thumbnail: "https://tse2.mm.bing.net/th/id/OIP.IrUBHhdMo6wWLFueKNreRwHaHa?cb=12&rs=1&pid=ImgDetMain&o=7&rm=3",
     author: "Rajesh Mehra",
     proffection: "CEO, InnovateCorp",
     para: "UCU is bridging the gap between academia and industry with programs that prepare future-ready leaders",
-  },
-  {
-    videoUrl: "https://youtu.be/bhgVNTJUytA?si=FGAtsM2xOS8NPyRU",
-    thumbnail: "https://www.spjimr.org/wp-content/uploads/2022/09/PGP-GM-1.jpg",
-    author: "Anita Kapoor",
-    proffection: "Managing Director, Visionary Enterprises",
-    para: "The corporate partnership initiatives at UCU are creating impactful opportunities for students and professionals alike.",
-  },
-  {
-    videoUrl: "https://youtu.be/ulaQhIpWY98?si=O_Ofh6g_EBIawX-C",
-    thumbnail: "https://www.spjimr.org/wp-content/uploads/2022/09/PGP-GM-2.jpg",
-    author: "Vikram Sethi",
-    proffection: "Chief Human Resources Officer, Global Dynamics",
-    para: "UCU is cultivating the next generation of business leaders through practical, industry-focused learning.",
   },
 ];
 
@@ -88,7 +74,6 @@ function OverviewParticipant() {
             <img
               src={item.thumbnail}
               alt={item.author}
-              style={{ width: "100%", cursor: "pointer" }}
               onClick={() => setModalVideo(item.videoUrl)}
               className="participant-play-thumbnail"
             />
