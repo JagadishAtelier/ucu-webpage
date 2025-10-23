2b8fb634b367b89908d5ea06e93c281fe0cc4c55
JagadishAtelier <jagadish.atelier@gmail.com>
2025-10-23 16:28:21 +0530

    updated mobile nav text trabsition

--- src/App.jsx
+++ src/App.jsx
@@ -70,7 +70,7 @@ function App() {
         <Route path="/csr" element={<SociatalImpact/>} />
         <Route path="/open-programs" element={<OpenPrograms/>} />
         <Route path="/summer-programs" element={<SummerProgram/>} />
-        <Route path="/admission" element={<AdmissionPage/>} />
+        <Route path="/admissions" element={<AdmissionPage/>} />
         {/* <Route path="/account" element={<ProfilePage />} /> */}
       </Routes>
       <WhatsAppButton/>
---
+++ src/Pages/CareersPage/CareersPageOpening.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CareersPageOpening() {
+  return (
+    <div>
+        <h1>Current Job Openings</h1>
+    </div>
+  )
+}
+
+export default CareersPageOpening
