293ecf219c2b72dec467dbce3ee4563c18eeb078
JagadishAtelier <jagadish.atelier@gmail.com>
2025-12-02 18:25:24 +0530

    worked on executive page

--- src/Pages/LeaderShipCoachPage/LCPBanner.jsx
+++ src/Pages/LeaderShipCoachPage/LCPBanner.jsx
@@ -1,25 +1,20 @@
 import React from "react";
 import "./LCPBanner.css";
-import Navbar from "../../Components/Navbar/Navbar";
-import NewFooter from "../../Components/NewFooter/NewFooter";
 import LCPPower from "./LCPPower";
-import LCPEnrollment from "./LCPEnrollment";
 import LCPGrid from "./LCPGrid";
 import ClientProfile from "./ClientProfile";
-import OurExperts from "./ourExperts";
 import LCPWhy from "./LCPWhy";
 import LCPContact from "./LCPContact";
-
+import OurExperts from './OurExperts'
 function LCPBanner() {
     return (
         <div className='captital-campus-content-sec mt-4'>
-            <LCPPower/>
-            {/* <LCPEnrollment/> */}
-            <LCPGrid/>
-            <ClientProfile/>
+            <LCPPower />
+            <LCPGrid />
+            <ClientProfile />
             <OurExperts/>
-            <LCPWhy/>
-            <LCPContact/>
+            <LCPWhy />
+            <LCPContact />
         </div>
     );
 }
